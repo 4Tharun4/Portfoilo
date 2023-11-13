fb5326d68564e0321ea0b866e671c1dad137878f
4tharun4 <tharungv4@gmail.com>
2023-11-13 21:00:47 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import { Navbar } from "./Components/NavBar/NavBar";
+
+
+function App() {
+  return(
+    <Navbar/>
+  )
+}
+
+export default App;
---
+++ src/Components/NavBar/NavBar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './NavBar.css'
+
+
+export const Navbar =()=>{
+    return(
+       <nav className="main">
+        <img src="" alt=""/>
+        
+       </nav>
+    )
+}
